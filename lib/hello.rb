14f0923beddcf131559aed82a55c266cfbafea9d
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-05-06 13:20:50 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,19 +1,1 @@
-def hello_t(array)
-  if block_given?
-  i = o;
 
-  while i < array.length
-    yield array[i]
-    i = i + 1
-  end
-  array
-  else
-    puts "Hey! No block was given!"
-  end
-end
-
-["Tim", "Tom", "Jim"].each do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
